650c5af05152f63556d0c607b0b32b43749659fa
Sahib.Aujla <sahibpreet568@gmail.com>
2025-08-22 13:00:02 +0530

    completed checkpoint 2

--- packages/hardhat/contracts/PredictionMarket.sol
+++ packages/hardhat/contracts/PredictionMarket.sol
@@ -68,6 +68,14 @@ contract PredictionMarket is Ownable {
     //////////////////
     ////Constructor///
     //////////////////
+    address public immutable i_oracle;
+    uint256 public immutable i_initialTokenValue;
+    uint256 public immutable i_percentageLocked;
+    uint256 public immutable i_initialYesProbability;
+
+    string public s_question;
+    uint256 public s_ethCollateral;
+    uint256 public s_lpTradingRevenue;
 
     constructor(
         address _liquidityProvider,
@@ -79,6 +87,24 @@ contract PredictionMarket is Ownable {
     ) payable Ownable(_liquidityProvider) {
         /// Checkpoint 2 ////
         /// Checkpoint 3 ////
+        if (msg.value == 0) {
+            revert PredictionMarket__MustProvideETHForInitialLiquidity();
+        }
+        if (_initialYesProbability >= 100 || _initialYesProbability == 0) {
+            revert PredictionMarket__InvalidProbability();
+        }
+
+        if (_percentageToLock >= 100 || _percentageToLock == 0) {
+            revert PredictionMarket__InvalidPercentageToLock();
+        }
+
+        i_oracle = _oracle;
+        s_question = _question;
+        i_initialTokenValue = _initialTokenValue;
+        i_initialYesProbability = _initialYesProbability;
+        i_percentageLocked = _percentageToLock;
+
+        s_ethCollateral = msg.value;
     }
 
     /////////////////
